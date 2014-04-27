1800f4fbc6dd834972ef674fb6fbfe13a1a212cb
Umaer <atumair@gmail>
2014-04-27 16:42:50 +0500

    my code

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 mypearassignment
 ================
+
+You can run script by calling source("run_analysis.R")
+
+Make sure to set working directory first before calling the script
+
+The script reads data from directory Samsung data so make sure it is not blank
+
+Samsung data must contain UCI HAR Dataset and UCI HAR Dataset must contain test and train directory and other files which we all downloaded for implementing this task
+
+source code is tested and documented properly
+
+The source code is doing the following task
+
+1) Reads all test, train, features, activities data
+2) It combines all data into one big dataframe
+3) assigs column names to the dataframe
+4) creates column for activity name in the dataset
+5) extracts mean, std, subject and output columns from dataset
+6) creates tidy data set with the average of each variable for each activity and each subject
+7) write the tidy data into tidydata.txt in current working directory
--- run_analysis.R
+++ run_analysis.R
@@ -3,25 +3,25 @@
 rm(list = setdiff(ls(), lsf.str()))
 
 ##reading all training data
-X_train <- read.table("D:/DataScience/CleaningData/Week3/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt", quote="\"")
+X_train <- read.table("Samsung data/UCI HAR Dataset/train/X_train.txt", quote="\"")
 
-y_train <- read.table("D:/DataScience/CleaningData/Week3/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt", quote="\"")
+y_train <- read.table("Samsung data/UCI HAR Dataset/train/y_train.txt", quote="\"")
 
-subject_train <- read.table("D:/DataScience/CleaningData/Week3/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt", quote="\"")
+subject_train <- read.table("Samsung data/UCI HAR Dataset/train/subject_train.txt", quote="\"")
 
 ##reading all testing data
-X_test <- read.table("D:/DataScience/CleaningData/Week3/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt", quote="\"")
+X_test <- read.table("Samsung data/UCI HAR Dataset/test/X_test.txt", quote="\"")
 
-y_test <- read.table("D:/DataScience/CleaningData/Week3/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt", quote="\"")
+y_test <- read.table("Samsung data/UCI HAR Dataset/test/y_test.txt", quote="\"")
 
-subject_test <- read.table("D:/DataScience/CleaningData/Week3/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt", quote="\"")
->   View(subject_test)
+subject_test <- read.table("Samsung data/UCI HAR Dataset/test/subject_test.txt", quote="\"")
+
 
 ##reading features
-features <- read.table("D:/DataScience/CleaningData/Week3/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/features.txt", quote="\"")
+features <- read.table("Samsung data/UCI HAR Dataset/features.txt", quote="\"")
 
 ##reading actvity labels
-act_labels <- read.table("D:/DataScience/CleaningData/Week3/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt", quote="\"")
+act_labels <- read.table("Samsung data/UCI HAR Dataset/activity_labels.txt", quote="\"")
 
 ## labeling traing and testing data
 names(X_test) = as.character((features[,2]))
